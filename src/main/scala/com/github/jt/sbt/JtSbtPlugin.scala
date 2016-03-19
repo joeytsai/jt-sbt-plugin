8b3f82d97a56f904f665b0bc55c53c5e0f401cee
joeytsai <joeytsai@gmail.com>
2016-03-19 16:34:58 -0700

    add scala options

--- src/main/scala/com/github/jt/sbt/JtSbtPlugin.scala
+++ src/main/scala/com/github/jt/sbt/JtSbtPlugin.scala
@@ -9,7 +9,22 @@ object JtSbtPlugin extends AutoPlugin {
 
   private lazy val jtSettings = Seq(
     organization := "com.github.jt",
-    scalaVersion := "2.11.7"
+    scalaVersion := "2.11.7",
+    // Scala 2.11 incremental compilation
+    incOptions := incOptions.value.withNameHashing(true),
+    // Don't publish javadoc or sources
+    publishArtifact in (Compile, packageDoc) := false,
+    publishArtifact in (Compile, packageSrc) := false,
+
+    scalacOptions := Seq(
+      "-encoding", "UTF-8",
+      "-deprecation",
+      "-unchecked",
+      "-feature",
+      "-target:jvm-1.8",
+      "-Xlint",
+      "-Xfatal-warnings"
+    )
   )
 
 }
